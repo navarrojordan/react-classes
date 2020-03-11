d220d12a24746efa048242c54ea5ca7d44386a6f
Jordan Navarro <jordan.navarro1@live.com>
2020-03-10 21:48:19 -0300

    adicionando a lista 1

---
+++ exercicio.js
@@ -0,0 +1,58 @@
+// Declarar uma variável chamada `myvar`, sem valor.
+var myVar;
+
+// Após declarada, atribua o valor 10 à variável `myvar`.
+
+myVar = 10;
+
+// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+
+var num = 15
+var num2 =  8; 
+
+soma = num + num2;
+
+// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+
+var soma = 23;
+soma  + 1;
+
+
+// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+
+var soma = 23;
+soma * 3;
+
+// Qual é o valor da variável `soma` até aqui?
+
+var soma = 69;
+
+
+// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+
+var souninja = Boolean;
+
+
+// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+
+let comida = [arroz, feijao, ovo];
+
+
+// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+
+let comida = [arroz, feijao, ovo];
+
+
+// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
+
+// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
+
+// Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+
+
+
+
+
+
